411cf035a68220b887c86db04454af69c4237550
Muthu lakshmi <muthulakshmi180203@gmail.com>
2025-12-24 17:42:03 +0530

    Frontend ready for Netlify

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,7 +1,7 @@
 // frontend/src/services/taskService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/tasks';
+const API_BASE_URL = 'https://yamaha-task-management.onrender.com';
 
 const taskService = {
     getAllTasks: () => axios.get(API_BASE_URL),
